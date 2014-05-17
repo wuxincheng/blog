ee7c00ccd644b397aa07dc1e0240227ccc9b6910
Xin-Cheng Wu <wuxinchenghappy@gmail.com>
2014-05-17 12:12:21 +0800

    modify

---
+++ src/main/java/com/wuxincheng/blog/util/Constants.java
@@ -0,0 +1,14 @@
+package com.wuxincheng.blog.util;
+
+/**
+ * 常量
+ * 
+ * @author wuxincheng
+ *
+ */
+public class Constants {
+
+	/** 导航Active常量 */
+	public static final String TOP_NAV_FLAG = "navFlag";
+	
+}
